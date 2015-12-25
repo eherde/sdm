02d13ea2f55a53e0d88dc7f4ec6d6f84978011c0
Eric Herde <eherde@users.noreply.github.com>
2015-12-24 22:24:54 -0800

    implement stop
    * stop the process from the pidfile
    * remove the pidfile

--- src/sdm/stop.c
+++ src/sdm/stop.c
@@ -1,4 +1,32 @@
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "options.h"
+
+extern options opts;
+extern char *g_pidfile;
+extern char *g_root;
+
+extern char *set_pidfile(const char *root, const char *cmd);
+extern char *set_root(const char *cmd);
+
+extern pid_t read_pidfile(const char *path);
+
 int stop(void)
 {
-	return 0;
+	int rv = -1;
+	pid_t pid = 0;
+	g_root = set_root(opts.path);
+	g_pidfile = set_pidfile(g_root, opts.cmd);
+	pid = read_pidfile(g_pidfile);
+	if (pid <= 0) { /* could not find pid in pidfile, try to find it in /proc */
+		goto out;
+	}
+	if (pid > 1) {
+		rv = kill(pid, SIGTERM);
+	}
+	unlink(g_pidfile);
+out:
+	return rv;
 }
